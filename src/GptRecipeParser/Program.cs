cca1607f448f496212f5e95f4838f019116706a3
Aurelien <a.cretaine@live.fr>
2025-01-06 23:12:52 +0100

    Initial Commit

---
+++ src/GptRecipeParser/Program.cs
@@ -0,0 +1,23 @@
+﻿using GptRecipeParser;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        try
+        {
+            (string result, decimal cout) = await DescriptionGenerator.GenerateDescription("");
+
+            Console.WriteLine(result);
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine($"Cost : {cout}");
+
+            Console.ReadKey();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred: {ex.Message}");
+        }
+    }
+}
